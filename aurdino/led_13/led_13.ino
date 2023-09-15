b31630fb92e6388f0aa1e57e94dda902e099906c
Mauricio <mauriacosta2012@gmail.com>
2023-09-15 11:47:38 -0500

    Semaforo con arduino

--- aurdino/led_13/led_13.ino
+++ aurdino/led_13/led_13.ino
@@ -1,13 +1,38 @@
+//Andres Acosta
 int led=13;
+int led2=6;
+int led3=2;
 void setup() {
   // put your setup code here, to run once:
-pinMode(led,OUTPUT);//Nr.pin 3 mode(output/input)
+pinMode(led,OUTPUT); //Nr.port 3 Mode
+pinMode(led2,OUTPUT);
+pinMode(led3,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ //led rojo
 digitalWrite(led,HIGH);
-delay(200); //1000=1seg
-digitalWrite(led,LOW);
-delay(200);
+delay(1000); //200=1s
+digitalWrite(13,LOW);
+delay(1000);
+//led amarillo
+digitalWrite(led2,HIGH);
+delay(1000); //200=1s
+digitalWrite(6,LOW);
+delay(800);
+//led verde
+digitalWrite(led3,HIGH);
+delay(1000); //200=1s
+digitalWrite(2,LOW);
+delay(1000);
+//led amarillo
+digitalWrite(led2,HIGH);
+delay(1000); //200=1s
+digitalWrite(6,LOW);
+delay(1000);
+ //led rojo
+digitalWrite(led,HIGH);
+delay(1000); //200=1s
+
 }
