bf226ffec7e14c2dba1942489c60bed7e8f65229
Mauricio <mauriacosta2012@gmail.com>
2023-09-15 12:21:16 -0500

    reto 3

---
+++ arduino/reto_3/reto_3.ino
@@ -0,0 +1,68 @@
+//Edilson Montilla Enriquez
+//Andres Mauricio Acosta
+
+int led=13;
+int led2=6;
+int led3=2;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT); //Nr.port 3 Mode
+pinMode(led2,OUTPUT);
+pinMode(led3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led, HIGH);
+digitalWrite(led3,HIGH);
+delay(1000); //200=1s
+digitalWrite(13, LOW);
+digitalWrite(2,LOW);
+delay(1000);
+digitalWrite(led, HIGH);
+digitalWrite(led3,HIGH);
+delay(1000); //200=1s
+digitalWrite(13, LOW);
+digitalWrite(2,LOW);
+delay(1000);
+digitalWrite(led, HIGH);
+digitalWrite(led3,HIGH);
+delay(1000); //200=1s
+digitalWrite(13, LOW);
+digitalWrite(2,LOW);
+delay(1000);
+digitalWrite(led, HIGH);
+digitalWrite(led3,HIGH);
+delay(1000); //200=1s
+digitalWrite(13, LOW);
+digitalWrite(2,LOW);
+delay(1000);
+digitalWrite(led, HIGH);
+digitalWrite(led3,HIGH);
+delay(1000); //200=1s
+digitalWrite(13, LOW);
+digitalWrite(2,LOW);
+delay(1000);
+//Led amarillo
+digitalWrite(led2, HIGH);
+delay(1000); //200=1s
+digitalWrite(6, LOW);
+delay(1000);
+digitalWrite(led2, HIGH);
+delay(1000); //200=1s
+digitalWrite(6, LOW);
+delay(1000);
+digitalWrite(led2, HIGH);
+delay(1000); //200=1s
+digitalWrite(6, LOW);
+delay(1000);
+digitalWrite(led2, HIGH);
+delay(1000); //200=1s
+digitalWrite(6, LOW);
+delay(1000);
+digitalWrite(led2, HIGH);
+delay(1000); //200=1s
+digitalWrite(6, LOW);
+delay(1000);
+
+}
